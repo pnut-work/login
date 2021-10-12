11988027854098816780963164bb9ba6cea7d786
pnut-work <ppeebb10@naver.com>
2021-10-12 15:30:00 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 5000
+const { User } = require('./models/user');
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+
+app.use(express.urlencoded({extended: true})) 
+app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://pnutwork:abcd1234@login.cqwnf.mongodb.net/login?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 // { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }
 ).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log('err'))
@@ -11,6 +17,18 @@ mongoose.connect('mongodb+srv://pnutwork:abcd1234@login.cqwnf.mongodb.net/login?
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
